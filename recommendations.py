ec0d3844499f7f34207fcab3ffcc95cbec504513
SophieTo <tovagliari.sophie@gmail.com>
2024-12-20 10:22:51 +0100

    wip: add parquet file with data and update recos file to use it

--- recommendations.py
+++ recommendations.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 
 
 def recos():
@@ -11,62 +12,43 @@ def recos():
 
 
 def search_recos(film):
+    #lecture du dataframe de recommandations
+    link = "dataframe_recommandation.parquet"
+    df = pd.read_parquet(link)
+   
+    if film:
+        # Recherche du film correspondant
+        if film in df['originalTitle'].values:
+            # Récupérer les informations des 5 films recommandés
+            recommendations = df.loc[df['originalTitle'] == film, 'nearest_neighbor_ids'].values[0]
+                       
+            # Créer une liste des données des films recommandés
+            results = []
+            for reco_title in recommendations[:5]:  # On limite à 5 films
+                movie_info = df[df['tconst'] == reco_title].iloc[0].to_dict()
+                results.append(movie_info)
+    else:
+        st.error("Film non trouvé dans la base de données.")
+        results = []
+
+    # Étape 2 : Afficher les films dans des colonnes
+    if results:
+        st.subheader(f"Recommandations pour : {film}")
+        cols = st.columns(len(results))  # Créer une colonne par film
+        
+        for col, movie in zip(cols, results):
+            with col:
+                st.image(movie['poster_path'], use_column_width=True)
+                st.markdown(f"**{movie['originalTitle']}**")
+                st.markdown(f"⭐ Note : {movie['averageRating']} (Nombre de votes : {movie['numVotes']})")
+                st.markdown(f"Résumé : {movie['overview']}")
+                st.markdown(f"Genre : {movie['genres']}")
+
+
+
     # TODO : algo qui retourne les résultats
     # TODO : gérer le cas des films sans image avec un placeholder
     # TODO : use difflib for approximation
-    results = [
-        {
-            "image": "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/fbxQ44VRdM2PVzHSNajUseUteem.jpg",
-            "title": "Harry Potter à l'école des sorciers",
-            "note": "7,7",
-            "nb_votes": "884k",
-            "summary": "Orphelin, le jeune Harry Potter peut enfin quitter ses tyranniques oncle et tante Dursley lorsqu'un curieux messager lui révèle qu'il est un sorcier. À 11 ans, Harry va enfin pouvoir intégrer la légendaire école de sorcellerie de Poudlard, y trouver une famille digne de ce nom et des amis, développer ses dons, et préparer son glorieux avenir.",
-            "genre": "Aventure, Fantastique",
-        },
-        {
-            "image": "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/8KpHRokGpiaqEGpjYe0rpywtvUx.jpg",
-            "title": "Harry Potter et la Chambre des secrets",
-            "note": "7,7",
-            "nb_votes": "884k",
-            "summary": "Orphelin, le jeune Harry Potter peut enfin quitter ses tyranniques oncle et tante Dursley lorsqu'un curieux messager lui révèle qu'il est un sorcier. À 11 ans, Harry va enfin pouvoir intégrer la légendaire école de sorcellerie de Poudlard, y trouver une famille digne de ce nom et des amis, développer ses dons, et préparer son glorieux avenir.",
-            "genre": "Aventure, Fantastique",
-        },
-        {
-            "image": "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/t4P2079IyK19njHDP2GwQrKdvzd.jpg",
-            "title": "Harry Potter et le Prisonnier d'Azkaban",
-            "note": "7,7",
-            "nb_votes": "884k",
-            "summary": "Orphelin, le jeune Harry Potter peut enfin quitter ses tyranniques oncle et tante Dursley lorsqu'un curieux messager lui révèle qu'il est un sorcier. À 11 ans, Harry va enfin pouvoir intégrer la légendaire école de sorcellerie de Poudlard, y trouver une famille digne de ce nom et des amis, développer ses dons, et préparer son glorieux avenir.",
-            "genre": "Aventure, Fantastique",
-        },
-        {
-            "image": "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/hBak1pn5pbI4ycAbrgMMn1YI7P1.jpg",
-            "title": "Harry Potter et la Coupe de feu",
-            "note": "7,7",
-            "nb_votes": "884k",
-            "summary": "Orphelin, le jeune Harry Potter peut enfin quitter ses tyranniques oncle et tante Dursley lorsqu'un curieux messager lui révèle qu'il est un sorcier. À 11 ans, Harry va enfin pouvoir intégrer la légendaire école de sorcellerie de Poudlard, y trouver une famille digne de ce nom et des amis, développer ses dons, et préparer son glorieux avenir.",
-            "genre": "Aventure, Fantastique",
-        },
-        {
-            "image": "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/9ZfpCVNx0y8jpColnnfdA1HI4Zb.jpg",
-            "title": "Harry Potter et l'Ordre du Phénix",
-            "note": "7,7",
-            "nb_votes": "884k",
-            "summary": "Orphelin, le jeune Harry Potter peut enfin quitter ses tyranniques oncle et tante Dursley lorsqu'un curieux messager lui révèle qu'il est un sorcier. À 11 ans, Harry va enfin pouvoir intégrer la légendaire école de sorcellerie de Poudlard, y trouver une famille digne de ce nom et des amis, développer ses dons, et préparer son glorieux avenir.",
-            "genre": "Aventure, Fantastique",
-        },
-    ]
-    container = st.container(height=None, key="reco")
-    with container:
-        col1, col2, col3, col4, col5 = st.columns(5)
-        columns_list = [col1, col2, col3, col4, col5]
-        for i in range(5):
-            with columns_list[i]:
-                st.image(results[i]['image'])
-                st.markdown(results[i]['title'])
-                st.markdown(results[i]['note']+'-'+results[i]['nb_votes'])
-                st.markdown(results[i]['summary'])
-                st.markdown(results[i]['genre'])
-
+    
 
 recos()
