126302521060e65a41a825b358e6c6198a34e1b5
eldhan <eldhaen@proton.me>
2025-01-07 11:48:05 +0100

    feature: change homepage image

--- home.py
+++ home.py
@@ -5,7 +5,4 @@ st.title("23 v'là les films !")
 st.header(
     "Découvrez des films qui vous ressemblent : votre prochaine soirée cinéma commence ici, avec des recommandations sur mesure !"
 )
-st.image(
-    "assets/cin-plein-air-lille_1_2000.jpg",
-    caption="Source : lebonbon.com",
-)
+st.image("assets/bienvenue-en-creuse.png")
