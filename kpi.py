d1cecc35ba8523bbe459e7eca4195609cf5cf2c2
eldhan <eldhaen@proton.me>
2025-01-07 11:11:05 +0100

    feature: add powerbi dashboard to KPI page

--- kpi.py
+++ kpi.py
@@ -5,3 +5,7 @@ st.title("Indicateurs des performances")
 st.write(
     "Présentation de l’identification des acteurs les plus présents et les périodes associées, l’évolution de la durée moyenne des films au fil des années, la comparaison entre les acteurs présents au cinéma et dans les séries, l’âge moyen des acteurs, ainsi que les films les mieux notés et les caractéristiques qu’ils partagent."
 )
+st.markdown(
+    '<iframe class="iframe_powerbi"  title="projet2_powerbi" width="600" height="636" src="https://app.powerbi.com/view?r=eyJrIjoiYTg1NmQyNGYtYTlkYS00ZmI2LWI2MmQtZDYwZTNlZjk1YzJjIiwidCI6ImRjZjlkYjM3LTc0MGUtNDZkZS04NTFiLWQ1YTE1MGNlYmVhNSJ9&pageName=a0b29f7e9243c02194c0" frameborder="0" allowFullScreen="true"></iframe>',
+    unsafe_allow_html=True,
+)
